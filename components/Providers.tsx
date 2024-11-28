70eae9a715e6503404bf12eb89308e9656db918c
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2024-11-28 19:20:55 +0100

    add fix

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,8 +1,17 @@
 "use client"
 
 import { Provider } from 'react-redux'
-import { store } from '@/lib/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from '@/lib/store'
+import { LayoutLoader } from './layout/loading'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>
+  return (
+    <Provider store={store}>
+      {/* Delay rendering until the persisted state is rehydrated */}
+      <PersistGate loading={<LayoutLoader/>} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  )
 }
--- lib/store.ts
+++ lib/store.ts
@@ -1,15 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from './slices/authSlice'
-import inventoryReducer from './slices/inventorySlice'
-import ordersReducer from './slices/ordersSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // This uses localStorage by default
+import authReducer from "./slices/authSlice";
+import inventoryReducer from "./slices/inventorySlice";
+import ordersReducer from "./slices/ordersSlice";
+import { combineReducers } from "redux";
 
+// Redux Persist Config
+const persistConfig = {
+	key: "root", // Key to store the Redux state
+	storage, // You can use 'sessionStorage' instead of 'localStorage' if you prefer
+	whitelist: ["auth"], // Specify which slices you want to persist (e.g., auth)
+};
+
+// Combine reducers
+const rootReducer = combineReducers({
+	auth: authReducer,
+	inventory: inventoryReducer,
+	orders: ordersReducer,
+});
+
+// Create a persisted reducer using the config
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create the store with the persisted reducer
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    inventory: inventoryReducer,
-    orders: ordersReducer,
-  },
-})
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: ["persist/PERSIST"], // Ignore specific actions
+			},
+		}),
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+// Create a persistor to manage the persistence of the store
+export const persistor = persistStore(store);
+
+// TypeScript types
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
