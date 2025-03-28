64848aa8f16fe59e140343bc94c4a0de70724118
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2025-03-28 18:05:18 +0100

    modified:   components/settings/UserPreferences.tsx

--- components/settings/UserPreferences.tsx
+++ components/settings/UserPreferences.tsx
@@ -1,16 +1,29 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Sun, Bell, SunMoon, Settings2 } from 'lucide-react';
+import { Sun, Bell, SunMoon, Settings2, FlaskConical } from 'lucide-react';
 
 export function UserPreferences() {
   const { theme, setTheme } = useTheme();
   const [notifications, setNotifications] = useState(true);
+  const [isDemo, setIsDemo] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Load demo toggle state from local storage
+    const demoState = localStorage.getItem("isLive");
+    setIsDemo(demoState === "false");
+  }, [isDemo]);
+
+  const handleToggle = () => {
+    const newState = !isDemo;
+    setIsDemo(newState);
+    localStorage.setItem("isLive", (!newState).toString());
+  };
 
   return (
     <Card className="">
@@ -23,10 +36,10 @@ export function UserPreferences() {
       </CardHeader>
       <CardContent className="space-y-6">
         <div className="flex flex-col space-y-2">
-        <div className="flex items-center space-x-2">
+          <div className="flex items-center space-x-2">
             <SunMoon className="h-5 w-5" />
             <Label htmlFor="theme" className="text-lg">Theme</Label>
-            </div>
+          </div>
           <Select value={theme} onValueChange={(value) => setTheme(value)}>
             <SelectTrigger className="w-full">
               <SelectValue placeholder="Select a theme" />
@@ -47,6 +60,19 @@ export function UserPreferences() {
             id="notifications"
             checked={notifications}
             onCheckedChange={setNotifications}
+          />
+        </div>
+
+        {/* Demo Toggle */}
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-2">
+            <FlaskConical className="h-5 w-5" />
+            <Label htmlFor="demo" className="text-lg">Demo Mode</Label>
+          </div>
+          <Switch
+            id="demo"
+            checked={isDemo}
+            onCheckedChange={handleToggle}
           />
         </div>
       </CardContent>
